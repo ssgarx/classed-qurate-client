34f939e314385f74598a73ab2af477407008506f
ssgarx <sagarchandgadkar74@gmail.com>
2021-10-04 22:35:35 +0530

    new server

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://young-reaches-51472.herokuapp.com/",
+  uri: "https://mysterious-anchorage-33929.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
